ec7229a503eabf68b760665fab92290f627949b4
zoff <mzoffm17@student.aau.dk>
2022-04-28 12:40:02 +0200

    update

--- p10/Assets/Scripts/HammerDialogScript.cs
+++ p10/Assets/Scripts/HammerDialogScript.cs
@@ -20,6 +20,7 @@ public class HammerDialogScript : MonoBehaviour
     bool lvl2Banter1 = false;
     bool lvl2Banter2 = false;
     bool lvl3CutSceneDone = false;
+    int i = 0;
     AudioSource sound;
     [SerializeField] private List<AudioClip> audioTrack;
     void Start()
@@ -33,18 +34,20 @@ public class HammerDialogScript : MonoBehaviour
         if (th.getPlayHammerIntroVoiceLine() && !hammerIntro && !sound.isPlaying)
         {
             // play hammer intro dialog
-            playSound(0);
+            playSound(i);
             hammerIntro = true;
         }
 
         if (hammerIntro && !lvl1Intro && !sound.isPlaying)
         {
             // quest 1 intro
+            i++;
             playSound(1);
             lvl1Intro = true;
         }
         if (mqc.getLvl1Active() && !lvl1Active && !sound.isPlaying)
         {
+            i++;
             // play trapped fire spirit dialog
             playSound(2);
             lvl1Active = true;
@@ -52,36 +55,42 @@ public class HammerDialogScript : MonoBehaviour
         // banter
         if (walk1.getBanter1() && !lvl1Banter && !sound.isPlaying)
         {
+            i++;
             // player lvl 1 Banter
             playSound(3);
             lvl1Banter = true;
         }
         if (mqc.getLvl1Complete() && !playLvl2Intro && !sound.isPlaying)
         {
+            i++;
             // play intro to lvl 2
             playSound(4);
             playLvl2Intro = true;
         }
         if (q2.getChurchComplete() && !playLvl2TrappedSpirit && !sound.isPlaying)
         {
+            i++;
             // play trapped firespirit 2
             playSound(5);
             playLvl2TrappedSpirit = true;
         }
         if (walk2.getBanter1() && !lvl2Banter1 && !sound.isPlaying)
         {
+            i++;
             // player lvl 2.1 Banter
             playSound(6);
             lvl2Banter1 = true;
         }
         if (walk2.getBanter2() && !lvl2Banter2 && !sound.isPlaying)
         {
+            i++;
             // player lvl 2.2 Banter
             playSound(7);
             lvl2Banter2 = true;
         }
         if (bcs.getCutSceneDone() && !lvl3CutSceneDone && !sound.isPlaying)
         {
+            i++;
             // voiceline f�rdig med cutscene
             playSound(8);
             lvl3CutSceneDone = true;
@@ -102,7 +111,7 @@ public class HammerDialogScript : MonoBehaviour
             isPlaying = true;
 
             // audioSource.pitch = picth;
-            sound.PlayOneShot(audioTrack[index]);
+            sound.PlayOneShot(audioTrack[i]);
             sound.Play();
         }
 
